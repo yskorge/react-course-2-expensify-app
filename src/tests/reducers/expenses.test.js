9e53af4c87ca80a5024ecc39aa8d283b998bad00
yskorge <yskorge@gmx.de>
2019-04-24 15:40:39 +0200

    remove expense firebase

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -1,5 +1,6 @@
 import expensesReducer from '../../reducers/expenses';
 import expenses from '../fixtures/expenses';
+import { startRemoveExpense } from '../../actions/expenses';
 
 test('should set default state', () => {
   const state = expensesReducer(undefined, { type: '@@INIT' });
